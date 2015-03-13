a3338dae6dc24a09fb29edf04eb1a0e288adcf49
Justin Redd <justinredd@gmail.com>
2015-03-13 11:44:08 -0700

    Added output to see if we're running chef-solo vs. chef-zero

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,8 +4,12 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-nodes = search(:node, "hostname:[* TO *] AND chef_environment:#{node.chef_environment}")
+# nodes = search(:node, "hostname:[* TO *] AND chef_environment:#{node.chef_environment}")
 
-log 'foo' do
+log "Is chef solo? #{Chef::Config[:solo]}" do
+  level :warn
+end
+
+log "Is chef zero? #{!!Chef::Config[:chef_zero_port]}" do
   level :warn
 end
